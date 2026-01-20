1911671d5af945b1f568ec57bc082967512099c2
Aaron Fleming <37069089+a-fleming@users.noreply.github.com>
2026-01-19 22:27:11 -0800

    feat(cli): add log command group

--- src/nextgame/cli.py
+++ src/nextgame/cli.py
@@ -18,6 +18,30 @@ def build_parser() -> argparse.ArgumentParser:
                              %(default)s)")
     init_parser.set_defaults(func=cmd_init)
 
+    # Define parsers for LOG area
+    log_parser = area_parsers.add_parser("log", help="operations on game \
+                                         sessions")
+    log_actions = log_parser.add_subparsers(dest="action", required=True)
+    log_add = log_actions.add_parser("add", help="add a session to the \
+                                     database")
+    log_add.add_argument("--game", help="name of the game", required=True)
+    log_add.add_argument("--date", required=True, type=str, help="date the \
+                         game was played (YYYY-MM-DD)")
+    log_add.add_argument("--players", required=True, type=int, help="number\
+                          of players")
+    log_add.add_argument("--time", required=True, type=int, help="duration \
+                         of the game (minutes)")
+    log_add.set_defaults(func=cmd_log_add)
+
+    log_delete = log_actions.add_parser("delete", help="delete a session \
+                                        from the log")
+    log_delete.add_argument("--id", required=True, type=int, help="id of \
+                             session")
+    log_delete.set_defaults(func=cmd_log_delete)
+
+    log_list = log_actions.add_parser("list", help="list all sessions")
+    log_list.set_defaults(func=cmd_log_list)
+
     # Define parsers for TAG area
     tag_parser = area_parsers.add_parser("tag", help="operations on tags")
     tag_actions = tag_parser.add_subparsers(dest="action", required=True)
@@ -49,6 +73,20 @@ def cmd_demo(args):
 def cmd_init(args):
     print("cmd_init()")
     print(f"db_path: {args.db_path}")
+
+def cmd_log_add(args):
+    print("cmd_log_add()")
+    print(f"game: {args.game}")
+    print(f"date: {args.date}")
+    print(f"players: {args.players}")
+    print(f"time: {args.time}")
+
+def cmd_log_delete(args):
+    print("cmd_log_delete()")
+    print(f"id: {args.id}")
+
+def cmd_log_list(args):
+    print("cmd_log_list()")
 
 def cmd_tag_add(args):
     print("cmd_tag_add()")
