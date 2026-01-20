d20b00518bf57a191e17ef0351ce5f449273118e
Aaron Fleming <37069089+a-fleming@users.noreply.github.com>
2026-01-19 19:33:47 -0800

    feat(cli): add tag command group

--- src/nextgame/cli.py
+++ src/nextgame/cli.py
@@ -5,18 +5,41 @@ def build_parser() -> argparse.ArgumentParser:
     area_parsers = parser.add_subparsers(dest="area", required=True)
     
     # Define parser for DEMO
-    demo_parser = area_parsers.add_parser("demo", help="Create a demo \
+    demo_parser = area_parsers.add_parser("demo", help="create a demo \
                                           database seeded with games, \
                                           tags, and sessions")
     demo_parser.set_defaults(func=cmd_demo)
 
     # Define parser for INIT area
-    init_parser = area_parsers.add_parser("init", help="Initialize nextgame \
+    init_parser = area_parsers.add_parser("init", help="initialize nextgame \
                                           database")
-    init_parser.add_argument("--db-path", default="nextgame.db", help="Path \
+    init_parser.add_argument("--db-path", default="nextgame.db", help="path \
                              to database file to use or create (default: \
                              %(default)s)")
     init_parser.set_defaults(func=cmd_init)
+
+    # Define parsers for TAG area
+    tag_parser = area_parsers.add_parser("tag", help="operations on tags")
+    tag_actions = tag_parser.add_subparsers(dest="action", required=True)
+    
+    tag_add = tag_actions.add_parser("add", help="add a tag to the database")
+    tag_add.add_argument("--tag", required=True, help="name of tag")
+    tag_add.set_defaults(func=cmd_tag_add)
+
+    tag_delete = tag_actions.add_parser("delete", help="delete a tag from \
+                                        the database")
+    tag_delete.add_argument("--tag", required=True, help="name of tag")
+    tag_delete.set_defaults(func=cmd_tag_delete)
+    
+    tag_game = tag_actions.add_parser("game", help="apply one or more tags \
+                                      to a game")
+    tag_game.add_argument("--game", required=True, help="name of game")
+    tag_game.add_argument("--tag", required=True, action="append",
+                          help="name of tag (use --tag TAG for each tag)")
+    tag_game.set_defaults(func=cmd_tag_game)
+
+    tag_list = tag_actions.add_parser("list", help="list all tags")
+    tag_list.set_defaults(func=cmd_tag_list)
 
     return parser
 
@@ -26,6 +49,22 @@ def cmd_demo(args):
 def cmd_init(args):
     print("cmd_init()")
     print(f"db_path: {args.db_path}")
+
+def cmd_tag_add(args):
+    print("cmd_tag_add()")
+    print(f"tag: {args.tag}")
+
+def cmd_tag_delete(args):
+    print("cmd_tag_delete()")
+    print(f"tag: {args.tag}")
+
+def cmd_tag_game(args):
+    print("cmd_tag_game()")
+    print(f"game: {args.game}")
+    print(f"tag(s): {args.tag}")
+
+def cmd_tag_list(args):
+    print("cmd_tag_list()")
 
 def main():
     parser = build_parser()
