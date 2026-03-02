91bbc1d50d36a4608c9238a3fd9fa6330a81cc23
Aaron Fleming <37069089+a-fleming@users.noreply.github.com>
2026-03-01 20:53:12 -0800

    fix(db): create parent dirs for db if needed

--- src/nextgame/db/connection.py
+++ src/nextgame/db/connection.py
@@ -9,6 +9,11 @@ logger = logging.getLogger(__name__)
 
 def get_connection(db_path: PathLike) -> sqlite3.Connection:
     logger.debug("get_connection() called with db_path=%r", db_path)
+    
+    # Create parent dirs for db if needed
+    db_path = Path(db_path).expanduser()
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+
     conn = sqlite3.connect(db_path)
     logger.info("Opened SQLite connection: %s", db_path)
     conn.row_factory = sqlite3.Row # have rows behave like dicts: row["col"]
